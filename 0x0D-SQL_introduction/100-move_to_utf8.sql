4cadc824f12951fcddb9071fe0422690011f5313
DaVinCi <52623427+Blue-Davinci@users.noreply.github.com>
2024-03-14 16:57:30 +0300

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,3 @@
 -- Change the DB char set to UF8
-ALTER TABLE
-      `first_table`
-      MODIFY `name`
-      VARCHAR(256)
-      CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
-
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
